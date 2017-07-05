b245a44723b20908fe4893ff1e8c1672a674d2a2
LeighBarnett <leighbarnett5@gmail.com>
2017-07-05 11:53:12 +0300

    Finished Server API

---
+++ goalModel.js
@@ -0,0 +1,28 @@
+var mongoose= require("mongoose");
+
+var Schema=mongoose.Schema
+
+var goalSchema = new Schema({
+    goal:{type: String},
+    deadline: {type: String},
+    category: {type: String},
+    image_url: {type: String},
+    importance:{type: Number},
+    urgency:{type: Number},
+    importanceRating: [Number],
+    urgencyRating: [Number],
+    percentageCompleted:{type: Number}
+})
+var Goal=mongoose.model('goal',goalSchema)
+
+var newGoal= new Goal({
+  goal:  "Finish this project",
+  deadline: "8 July 2017",
+  category: "Studies",
+  importance: 5,
+  urgency:4
+})
+
+// newGoal.save();
+
+module.exports=Goal;
